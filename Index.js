f5e2d24cc0c7937ce5138d3438693120a6c0e8c8
Ivan Pratt <ivandoublejr@gmail.com>
2022-03-24 12:13:41 -0600

    working on the slider

--- Index.js
+++ Index.js
@@ -1,1 +1,10 @@
 console.log("Working")
+
+const slider = document.getElementById("slider")
+
+
+
+
+slider.addEventListener('input', () => {
+    document.getElementById("meatmanualinput").value = slider.value
+})

ed0e4d3a9f988a05c831a8a4c62ed6686065a17d
Ivan Pratt <ivandoublejr@gmail.com>
2022-03-30 22:19:27 -0600

    done for the day, need to add the recipe finally

--- Index.js
+++ Index.js
@@ -5,12 +5,12 @@ let knifevariable = 0
 let jerkyGun = 0
 let previousMultiplier = 1
 
-let smokinessMultiplier = 0
-let spicinessMultiplier = 0
-let saltinessMultiplier = 0
-let sweetnessMultiplier = 0
-let toughnessMultiplier = 0
-let shelflifeMultiplier = 0
+let smokinessMultiplier = 1
+let spicinessMultiplier = 1
+let saltinessMultiplier = 0.9
+let sweetnessMultiplier = 1
+let toughnessMultiplier = 0.9
+let shelflifeMultiplier = 0.9
 
 let jerkyObject = {
     Smokiness: "",
@@ -25,7 +25,7 @@ let jerkyObject = {
     Postingredients: [],
     Spice: [],
     Sugar: [],
-    Items: [["Bowl", 1, "Small"], ["Plastic Wrap", 1, "Feet"], ["Knife", 1, "Medium"], ["Cutting Board", 1, "Small"], ["Jerky Gun", 1, "Medium"], ["Preparation Time", 1, "Hours"]]
+    Items: [["Bowl", 1, "Small"], ["Plastic Wrap", 1, "Feet"], ["Knife", 1, "Medium"], ["Cutting Board", 1, "Small"], ["Jerky Gun", 1, "Medium"], ["Preparation Time", 1, "Minutes"],  ["Marination Time", 1, "Hours"],  ["Dehydration Time", 1, "Hours"]]
 }
 
 let submitbutton1 = document.getElementById("submitButton")
@@ -35,6 +35,16 @@ const slider = document.getElementById("slider")
 let meatmanualinput = document.getElementById("meatmanualinput")
 let preparationdiv = document.getElementById("preparationdiv")
 let listcolumndivbase = document.getElementById("listcolumndivbase")
+let smokyItems = document.querySelectorAll(".smoky");
+let notsmokyItems = document.querySelectorAll(".notsmoky");
+let saltyItems = document.querySelectorAll(".salty");
+let spicyItems = document.querySelectorAll(".spicy");
+let sweetItems = document.querySelectorAll(".sweet");
+let toughItems = document.querySelectorAll(".tough");
+let tenderItems = document.querySelectorAll(".tender");
+let lowshelflifeItems = document.querySelectorAll(".lowshelf");
+
+
 
 let updateList = function(){
 
@@ -64,12 +74,18 @@ let updateList = function(){
     }
 
     for (const array of jerkyObject.Sugar){
-        let originalValue = array[1]
+        let originalValue = array[1] * sweetnessMultiplier
         array[1] = (originalValue / previousMultiplier) * meatmanualinput.value
-
+        sweetnessMultiplier = 1
     }
 
-    //jerkyObject.Items[5][1] = makeTime + cookTime + marinateTime 
+    makeTime = (meatmanualinput.value * 1) + 15
+    cookTime = 4 + (meatmanualinput.value * 0.05)
+    marinateTime = 12
+
+    jerkyObject.Items[5][1] = makeTime 
+    jerkyObject.Items[6][1] = marinateTime
+    jerkyObject.Items[7][1] = cookTime  
 
     if (bowlvariable === 1) {
     if (meatmanualinput.value <= 2){
@@ -124,6 +140,7 @@ let updateList = function(){
 
 const submitbutton1Function = function(){
     listcolumndivbase.innerHTML = '';
+    listcolumndivpost.innerHTML = '';
     preparationdiv.innerHTML = '';
 
     for (const item of jerkyObject.Meat){
@@ -145,21 +162,21 @@ const submitbutton1Function = function(){
         let itemptag = document.createElement("p");
         itemptag.classList.add("listItem")
         itemptag.innerHTML = (`${item[0]}: ${item[1].toFixed(2)} ${item[2]}`)
-        listcolumndivbase.appendChild(itemptag)
+        listcolumndivpost.appendChild(itemptag)
     }
 
     for (const item of jerkyObject.Spice){
         let itemptag = document.createElement("p");
         itemptag.classList.add("listItem")
         itemptag.innerHTML = (`${item[0]}: ${item[1].toFixed(3)} ${item[2]}`)
-        listcolumndivbase.appendChild(itemptag)
+        listcolumndivpost.appendChild(itemptag)
     }
 
     for (const item of jerkyObject.Sugar){
         let itemptag = document.createElement("p");
         itemptag.classList.add("listItem")
         itemptag.innerHTML = (`${item[0]}: ${item[1].toFixed(2)} ${item[2]}`)
-        listcolumndivbase.appendChild(itemptag)
+        listcolumndivpost.appendChild(itemptag)
     }
 
     for (let i = 0; i < 1; i++){
@@ -197,6 +214,16 @@ const submitbutton1Function = function(){
         itemptag.innerHTML = (`${jerkyObject.Items[5][1].toFixed()} ${jerkyObject.Items[5][2]} ${jerkyObject.Items[5][0]}`)
         preparationdiv.appendChild(itemptag)
 
+        itemptag = document.createElement("p");
+        itemptag.classList.add("listItem")
+        itemptag.innerHTML = (`${jerkyObject.Items[6][1].toFixed()} ${jerkyObject.Items[6][2]} ${jerkyObject.Items[6][0]}`)
+        preparationdiv.appendChild(itemptag)
+
+        itemptag = document.createElement("p");
+        itemptag.classList.add("listItem")
+        itemptag.innerHTML = (`${jerkyObject.Items[7][1].toFixed()} ${jerkyObject.Items[7][2]} ${jerkyObject.Items[7][0]}`)
+        preparationdiv.appendChild(itemptag)
+
     }
 
     for (const item of jerkyObject.Appliance){
@@ -204,6 +231,116 @@ const submitbutton1Function = function(){
         itemptag.classList.add("listItem")
         itemptag.innerHTML = (`${item[1].toFixed()} ${item[2]} ${item[0]}(s)`)
         preparationdiv.appendChild(itemptag)
+    }
+
+}
+
+let disableItems = function() {
+    if (smokinessMultiplier === 0){
+        for (i = 0; i < smokyItems.length; i++) {
+            smokyItems[i].disabled = true
+            smokyItems[i].checked = false
+
+        }
+    } else if (smokinessMultiplier === 5){
+        for (i = 0; i < smokyItems.length; i++) {
+            smokyItems[i].disabled = false
+            smokyItems[0].checked = true
+            
+        }
+        for (i = 0; i < notsmokyItems.length ; i++){
+            notsmokyItems[i].disabled = true
+            notsmokyItems[i].checked = false
+            
+        }
+    } else {
+        for (i = 0; i < notsmokyItems.length; i++) {
+            smokyItems[1].disabled = false
+            notsmokyItems[i].disabled = false
+        }
+        
+    }
+
+
+    if (saltinessMultiplier === 0){
+        for (i = 0; i < saltyItems.length; i++) {
+            saltyItems[i].disabled = true
+            saltyItems[i].checked = false
+
+        }
+    } else if (saltinessMultiplier === 1){
+        for (i = 0; i < saltyItems.length - 1; i++) {
+            saltyItems[i].disabled = false
+            saltyItems[4].checked = false
+            saltyItems[4].disabled = true
+        }
+    } else {
+        for (i = 0; i < saltyItems.length; i++) {
+            saltyItems[i].disabled = false
+        }
+    }
+
+    if (spicinessMultiplier === 0){
+        for (i = 0; i <  spicyItems.length; i++) {
+            spicyItems[i].disabled = true
+            spicyItems[i].checked = false
+
+        }
+    }else {
+        for (i = 0; i <  spicyItems.length; i++) {
+            spicyItems[i].disabled = false
+        }
+    }
+
+    if (sweetnessMultiplier === 0){
+        for (i = 0; i <  sweetItems.length; i++) {
+            sweetItems[i].disabled = true
+            sweetItems[i].checked = false
+
+        }
+    }else {
+        for (i = 0; i <  sweetItems.length; i++) {
+            sweetItems[i].disabled = false
+        }
+    }
+
+    if (toughnessMultiplier === 0){
+        for (i = 0; i < tenderItems.length; i++) {
+            tenderItems[i].disabled = false
+            tenderItems[5].checked = true
+        }
+
+    } else if (toughnessMultiplier === 1){
+        for (i = 0; i < tenderItems.length; i++) {
+            tenderItems[i].disabled = false
+            tenderItems[5].disabled = true
+            tenderItems[5].checked = false
+        }
+    } else if (toughnessMultiplier === 2) {
+        for (i = 0; i < tenderItems.length; i++) {
+            tenderItems[i].disabled = true
+            tenderItems[i].checked = false
+
+        }
+    }
+
+    if (shelflifeMultiplier === 0){
+        for (i = 0; i < lowshelflifeItems.length; i++) {
+            lowshelflifeItems[i].disabled = false
+        }
+
+    } else if (shelflifeMultiplier === 1){
+        for (i = 0; i < lowshelflifeItems.length; i++) {
+            lowshelflifeItems[i].disabled = true
+            lowshelflifeItems[i].checked = false
+            document.getElementById("nosweetness").checked = true
+        }
+    } else if (shelflifeMultiplier === 2) {
+        for (i = 0; i < lowshelflifeItems.length; i++) {
+            lowshelflifeItems[i].disabled = true
+            lowshelflifeItems[i].checked = false
+            document.getElementById("nosweetness").checked = true
+        }
     }
 
 }
@@ -227,41 +364,7 @@ const updateAllForms = function() {
     let shelflife = document.forms.shelflife.elements.shelflife.value
     jerkyObject.Shelflife = shelflife
 
-    let myappliancearray = Array.from(document.querySelectorAll("input[type=radio][name=appliance]:checked"), e => e.value.split(','));
-    for (const item of myappliancearray){
-        item[1] = parseFloat(item[1])
-    }
-    jerkyObject.Appliance = myappliancearray
-
-    let mymeatarray = Array.from(document.querySelectorAll("input[type=radio][name=meat]:checked"), e => e.value.split(','));
-    for (const item of mymeatarray){
-        item[1] = parseFloat(item[1])
-    }
-    jerkyObject.Meat = mymeatarray
-
-    let mybasearray = Array.from(document.querySelectorAll("input[type=checkbox][name=baseingredients]:checked"), e => e.value.split(','));
-    for (const item of mybasearray){
-        item[1] = parseFloat(item[1])
-    }
-    jerkyObject.Baseingredients = mybasearray
-
-    let mypostarray = Array.from(document.querySelectorAll("input[type=checkbox][name=postingredients]:checked"), e => e.value.split(','));
-    for (const item of mypostarray){
-        item[1] = parseFloat(item[1])
-    }
-    jerkyObject.Postingredients = mypostarray
-
-    let myspicearray = Array.from(document.querySelectorAll("input[type=checkbox][name=spice]:checked"), e => e.value.split(','));
-    for (const item of myspicearray){
-        item[1] = parseFloat(item[1])
-    }
-    jerkyObject.Spice = myspicearray
-
-    let mysugararray = Array.from(document.querySelectorAll("input[type=checkbox][name=sugar]:checked"), e => e.value.split(','));
-    for (const item of mysugararray){
-        item[1] = parseFloat(item[1])
-    }
-    jerkyObject.Sugar = mysugararray
+ 
 
     if (document.querySelectorAll("input[type=radio][value='Ground meats,1,lbs']:checked").length > 0){
         jerkyGun = 1
@@ -323,20 +426,61 @@ const updateAllForms = function() {
     }
 
     if(jerkyObject.Shelflife === "Years++"){
-        shelflifeMultiplier = 3
-    } else if (jerkyObject.Shelflife === "~Month"){
         shelflifeMultiplier = 2
-    } else if (jerkyObject.Shelflife === "2 Weeks"){
+    } else if (jerkyObject.Shelflife === "~Month"){
         shelflifeMultiplier = 1
+    } else if (jerkyObject.Shelflife === "2 Weeks"){
+        shelflifeMultiplier = 0
     } else if (jerkyObject.Shelflife === "Don't Care"){
         shelflifeMultiplier = 0
     }
-    
 
+    disableItems()
+
+    let myappliancearray = Array.from(document.querySelectorAll("input[type=radio][name=appliance]:checked"), e => e.value.split(','));
+    for (const item of myappliancearray){
+        item[1] = parseFloat(item[1])
+    }
+    jerkyObject.Appliance = myappliancearray
+
+    let mymeatarray = Array.from(document.querySelectorAll("input[type=radio][name=meat]:checked"), e => e.value.split(','));
+    for (const item of mymeatarray){
+        item[1] = parseFloat(item[1])
+    }
+    jerkyObject.Meat = mymeatarray
+
+    let mybasearray = Array.from(document.querySelectorAll("input[type=checkbox][name=baseingredients]:checked"), e => e.value.split(','));
+    for (const item of mybasearray){
+        item[1] = parseFloat(item[1])
+    }
+    jerkyObject.Baseingredients = mybasearray
+
+    let mypostarray = Array.from(document.querySelectorAll("input[type=checkbox][name=postingredients]:checked"), e => e.value.split(','));
+    for (const item of mypostarray){
+        item[1] = parseFloat(item[1])
+    }
+    jerkyObject.Postingredients = mypostarray
+
+    let myspicearray = Array.from(document.querySelectorAll("input[type=checkbox][name=spice]:checked"), e => e.value.split(','));
+    for (const item of myspicearray){
+        item[1] = parseFloat(item[1])
+    }
+    jerkyObject.Spice = myspicearray
+
+    let mysugararray = Array.from(document.querySelectorAll("input[type=checkbox][name=sugar]:checked"), e => e.value.split(','));
+    for (const item of mysugararray){
+        item[1] = parseFloat(item[1])
+    }
+    jerkyObject.Sugar = mysugararray
+    
+    
+    submitbutton1Function()
     console.log(jerkyObject)
    // console.log(makeTime)
     previousMultiplier = 1
 }
+
+
 
 
 //event listeners to change the Jerky Object
@@ -399,7 +543,6 @@ sugarform.addEventListener('change', () => {
 //resetbutton. Unchecks all checked inputs, then re-defines JerkyObject to empty again.
 resetbutton.addEventListener('click', () => {
     let allcheckboxes = Array.from(document.querySelectorAll("input[type=checkbox]:checked, input[type=radio]:checked"))
-    console.log(allcheckboxes)
     for (i = 0; i < allcheckboxes.length; i++){
         allcheckboxes[i].checked = false;
     }
@@ -416,9 +559,10 @@ resetbutton.addEventListener('click', () => {
         Postingredients: [],
         Spice: [],
         Sugar: [],
-        Items: [["Bowl", 1, "Small"], ["Plastic Wrap", 1, "Feet"], ["Knife", 1, "Medium"], ["Cutting Board", 1, "Small"], ["Jerky Gun", 1, "Medium"], ["Preparation Time", 1, "Hours"]]
+        Items: [["Bowl", 1, "Small"], ["Plastic Wrap", 1, "Feet"], ["Knife", 1, "Medium"], ["Cutting Board", 1, "Small"], ["Jerky Gun", 1, "Medium"], ["Preparation Time", 1, "Minutes"],  ["Marination Time", 1, "Hours"],  ["Dehydration Time", 1, "Hours"]]
     }
     bowlvariable = 0
+    submitbutton1Function()
 })
 
 
